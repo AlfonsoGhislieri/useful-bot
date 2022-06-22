6a329e5ccf0e17fe42e367fff5444212c20a49a1
Alfonso Ghislieri <alfonso.ghis@hotmail.fr>
2022-06-22 12:07:58 +0100

    moved bot to its own class

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,67 +1,61 @@
 import os
+from discord.ext import commands
 import random
 import discord
-from discord.ext import commands
 
-def main():
-  TOKEN = os.environ.get("DISCORD_TOKEN")
-  GUILD = os.environ.get("DISCORD_GUILD")
+class Bot(commands.Bot):
 
-  intents = discord.Intents.default()
-  intents.members = True
-  intents.messages = True
-  bot = commands.Bot(command_prefix='!',intents=intents)
+  def __init__(self, command_prefix, intents):
+    commands.Bot.__init__(self, command_prefix=command_prefix, intents=intents)
+    self.active_guild = os.environ.get("DISCORD_GUILD")
+    self.add_commands()
 
-  @bot.event
-  async def on_ready():
-    guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
+  async def on_ready(self):
+    guild = discord.utils.find(lambda g: g.name == self.active_guild , self.guilds)
 
     print(
-        f'{bot.user} is connected to the following guild:\n'
+        f'{self.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
-  @bot.event
-  async def on_member_join(member):
-    guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
+  async def on_member_join(self, member):
+    guild = discord.utils.find(lambda g: g.name == self.active_guild, self.guilds)
 
     await member.create_dm()
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to {guild.name}'
     )
 
-  @bot.event
-  async def on_message(message):
+  async def on_message(self, message):
     # guard if message is written by the bot
-    if message.author.id == bot.user.id:
+    if message.author.id == self.user.id:
       return
 
     possible_greetings = ["hello", "hey", "hi"]
 
-    if message.content in possible_greetings:
-      await message.channel.send(f'Hey <@{message.author.id}>')
-    await bot.process_commands(message)
+    if message.content.lower() in possible_greetings:
+      await message.channel.send(f'{message.content.capitalize()} <@{message.author.id}>')
+    await self.process_commands(message)
 
-  @bot.command(name="helper", help='bot comes to your aid')
-  async def on_message(ctx):
-    response = "I am here to help!"
-    await ctx.send(response)
+  def add_commands(self):
+    @self.command(name="helper", help='bot comes to your aid')
+    async def on_message(ctx):
+      response = "I am here to help!"
+      await ctx.send(response)
 
-  @bot.command(name='roll_dice', help='Simulates rolling dice.')
-  async def roll(ctx, number_of_dice : int, number_of_sides : int): 
-    max_dice = 100
-    min_sides = 2
+    @self.command(name='roll_dice', help='Simulates rolling dice.')
+    async def roll(ctx, number_of_dice : int, number_of_sides : int): 
+      max_dice = 100
+      min_sides = 2
 
-    if number_of_dice > max_dice:
-      return await ctx.send(f'Maximum number of dice capped at {max_dice} dice!')
+      if number_of_dice > max_dice:
+        return await ctx.send(f'Maximum number of dice capped at {max_dice} dice!')
 
-    if number_of_sides < min_sides:
-      return await ctx.send(f'Minimum number of sides must be {min_sides}!')
+      if number_of_sides < min_sides:
+        return await ctx.send(f'Minimum number of sides must be {min_sides}!')
 
-    dice = [
-        str(random.choice(range(1, number_of_sides + 1)))
-        for _ in range(number_of_dice)
-    ]
-    await ctx.send(', '.join(dice))
-
-  bot.run(TOKEN)
+      dice = [
+          str(random.choice(range(1, number_of_sides + 1)))
+          for _ in range(number_of_dice)
+      ]
+      await ctx.send(', '.join(dice))
---
+++ discord_bot/main.py
@@ -0,0 +1,13 @@
+import discord
+import os
+from discord_bot.bot import Bot
+
+def main():
+
+  TOKEN = os.environ.get("DISCORD_TOKEN")
+  intents = discord.Intents.default()
+  intents.members = True
+  intents.messages = True
+  bot = Bot(command_prefix='!', intents=intents)
+  bot.run(TOKEN)
+
