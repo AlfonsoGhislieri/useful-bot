52dc4a50fe126efd3d8a021ce08db410adb3bab9
Alfonso Ghislieri <alfonso.ghis@hotmail.fr>
2022-07-04 11:13:22 +0100

    added reaction intents and formatted main

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -2,12 +2,12 @@ import discord
 import os
 from discord_bot.bot import Bot
 
+
 def main():
-
-  TOKEN = os.environ.get("DISCORD_TOKEN")
-  intents = discord.Intents.default()
-  intents.members = True
-  intents.messages = True
-  bot = Bot(command_prefix='!', intents=intents)
-  bot.run(TOKEN)
-
+    TOKEN = os.environ.get("DISCORD_TOKEN")
+    intents = discord.Intents.default()
+    intents.members = True
+    intents.messages = True
+    intents.reactions = True
+    bot = Bot(command_prefix="!", intents=intents)
+    bot.run(TOKEN)
